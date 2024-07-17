3cf5ef8a740db36ab1a60215bfaa2e8948b54460
Ansh <anshkant090@gmail.com>
2024-07-17 22:19:49 +0530

    Used refs for input the usename

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,27 @@
+import { useState , useRef} from "react";
+
 export default function Player() {
+  // When you want a component to “remember” some information, 
+  // but you don’t want that information to trigger new renders, you can use a ref.
+  const playerName = useRef();
+  const [enterPlayerName,setPlayerName] = useState(null);
+
+  function handleClick(){
+    // here we are accessing the playerName ref current object which accesses the value of the input as we have
+    // set the ref prop right there in the input element and using the state change function to set the value of 
+    // enterPlayerName 
+    setPlayerName(playerName.current.value);
+    // now to clear the input we can use this syntax as playerName refers to the input element and we can change
+    // the value using particular propert(value)
+    playerName.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enterPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
