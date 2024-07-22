beb8dd884cffe6ec924319a7f62f23a069608eda
Ansh <anshkant090@gmail.com>
2024-07-22 18:30:16 +0530

    added the code to check score

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,33 +1,39 @@
-import { forwardRef,useImperativeHandle, useRef} from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 // this forwardRef as the name suggests is used to forward the props
-const ResultModal = forwardRef(function ResultModal({result, targetTime ,timeRemaining}, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset },ref) {
   const dialog = useRef();
+  // condition check for Lost if we are out of time
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);         // fixed the precision to 2 digits
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
-  // we are using this ref to expose those functions returned to other components. 
-  useImperativeHandle(ref,() => {
+  // we are using this ref to expose those functions returned to other components.
+  useImperativeHandle(ref, () => {
     // this fucntion will return the object that returns the functions to be exposed
-    return{
-      open(){
+    return {
+      open() {
         dialog.current.showModal();
-      }
+      },
     };
   });
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost!</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime}</strong> seconds.
       </p>
       <p>
-        You stopped the timer with <strong>{timeRemaining} seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
   );
-})
+});
 
 export default ResultModal;
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,66 +1,74 @@
 import { useRef, useState } from "react";
 import ResultModal from "./ResultModal";
 
-export default function TimerChallenge({titile, targetTimer}){
-    const timer = useRef();
-    const dialog = useRef();
+export default function TimerChallenge({ titile, targetTimer }) {
+  const timer = useRef();
+  const dialog = useRef();
 
-    const[timeRemaining, setTimeRemaining] = useState(targetTimer*1000);
+  const [timeRemaining, setTimeRemaining] = useState(targetTimer * 1000);
 
-    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTimer*1000;
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTimer * 1000;
 
-    // to manually delete our timer
-    if(timeRemaining <= 0){
-        clearInterval(timer.current);
-        setTimeRemaining(targetTimer*1000);
-        dialog.current.open();
-        console.log(timeRemaining);
-    }
+  // to manually delete our timer
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+    // console.log(timeRemaining);
+  }
 
-    // let timer;   // will store the timer pointer returned by the setTimeout
-    // we can not use this as it will lost the previous timer and on state change will refer to new one.
-    // so we use ref instead of normal behaviour.
-    function handleStart(){
-        
-        timer.current = setInterval(() => {     // it will return the pointer to the timer runnning
-            // prevTimeRemaining = this is the feature that it will automatically get the prev old time
-            // and we are deducting the 10 milisecodns during the state change
-            setTimeRemaining(prevTimeRemaining => prevTimeRemaining-10);
-        }, 10);
-        
-        // setTimerStarted(true);
-    }
+  function handleReset(){
+    setTimeRemaining(targetTimer * 1000);
+  }
 
-    function handleStop(){
-        // clear timeout function will take the pointer of the timer and stops the timer set by the setTimeout 
-        // clearTimeout(timer.current);
-        // and we have to use the clearInterval to stop the setInterval function
-        clearInterval(timer.current);
-        // setTimerStarted(false);
-        dialog.current.open();
-    }
+  // let timer;   // will store the timer pointer returned by the setTimeout
+  // we can not use this as it will lost the previous timer and on state change will refer to new one.
+  // so we use ref instead of normal behaviour.
+  function handleStart() {
+    timer.current = setInterval(() => {
+      // it will return the pointer to the timer runnning
+      // prevTimeRemaining = this is the feature that it will automatically get the prev old time
+      // and we are deducting the 10 milisecodns during the state change
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
 
-    return(
+    // setTimerStarted(true);
+  }
+
+  function handleStop() {
+    // clear timeout function will take the pointer of the timer and stops the timer set by the setTimeout
+    // clearTimeout(timer.current);
+    // and we have to use the clearInterval to stop the setInterval function
+    clearInterval(timer.current);
+    // setTimerStarted(false);
+    dialog.current.open();
+  }
+
+  return (
     <>
-        {/* so if timer got expired we will see the dialog box which shows the output */}
-        <ResultModal ref={dialog} result='lost' targetTime={targetTimer} timeRemaining={timeRemaining}/>
-        <section className="challenge">
-            <h2>{titile}</h2>
-            {/* {timeExpired && <p>You lost!</p>} */}
-            <p className="challenge-time">
-                {targetTimer} second{targetTimer > 1 ? 's' : ''}
-            </p>
-            <p>
-                {/* in onClick we have used the ternary check if our timer has starte then we have to 
+      {/* so if timer got expired we will see the dialog box which shows the output */}
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTimer}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
+      <section className="challenge">
+        <h2>{titile}</h2>
+        {/* {timeExpired && <p>You lost!</p>} */}
+        <p className="challenge-time">
+          {targetTimer} second{targetTimer > 1 ? "s" : ""}
+        </p>
+        <p>
+          {/* in onClick we have used the ternary check if our timer has starte then we have to 
                 point our onClick to handleStop function as it has started else to handleStart */}
-            <button onClick={timerIsActive ? handleStop : handleStart}>
-                    {timerIsActive ? 'Stop' :'Start'} Challenge
-                </button> 
-            </p>
-            <p className={timerIsActive ? 'active' : undefined}>
-                {timerIsActive ? 'Timer is running...' : 'Timer is inactive'}
-            </p>
-        </section>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Timer is running..." : "Timer is inactive"}
+        </p>
+      </section>
     </>
-    );
+  );
 }
